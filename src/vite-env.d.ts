410f324a8d1da6f2037af6051516ec41cbf3f7fe
Einzelgaanger <alfred@frontierfinance.org>
2026-07-24 15:03:27 +0300

    Fix Render TypeScript env/asset types and unblock vite build.

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,8 +6,40 @@ interface ImportMetaEnv {
   readonly VITE_NOTIFY_EMAIL_ENABLED?: string;
   readonly VITE_ENABLE_WALLET?: string;
   readonly VITE_ENABLE_ENGINE?: string;
+  readonly VITE_SHOW_DEMO?: string;
+  readonly DEV: boolean;
+  readonly PROD: boolean;
+  readonly MODE: string;
+  readonly SSR: boolean;
+  readonly BASE_URL: string;
 }
 
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
+declare module '*.css' {
+  const css: string;
+  export default css;
+}
+
+declare module '*.jpg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: string;
+  export default src;
 }
